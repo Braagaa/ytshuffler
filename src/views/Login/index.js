4b08d50b67272f668db4ae80a1f0ba5f8663ab64
Michael Braga <michael_braga@outlook.com>
2019-08-08 21:12:19 -0400

    added Merriweather Sans font

--- src/views/Login/index.js
+++ src/views/Login/index.js
@@ -2,9 +2,9 @@ import React from 'react';
 
 export default function (props) {
 	return (
-		<div class="container">
-			<div class="col">
-				<h1>YT Shuffler</h1>
+		<div className="container text-center">
+			<div className="col">
+				<h1 className="test">YT Shuffler</h1>
 			</div>
 		</div>	
 	);
