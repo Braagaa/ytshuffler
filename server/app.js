42b2afb3abb91ee71fc94745b046ca105fbabac1
Michael Braga <michael_braga@outlook.com>
2019-09-07 02:17:20 -0400

    API server created and is able to run

---
+++ server/app.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const path = require('path');
+const logger = require('morgan');
+const cookieParser = require('cookie-parser');
+const createError = require('http-errors');
+
+const app = express();
+
+app.set('env', process.env.NODE_ENV || 'DEV');
+app.set('port', process.env.PORT || '3001');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
+app.use(cookieParser());
+
+app.use((req, res, next) => next(createError(404)));
+
+app.use((err, req, res, next) => {
+	res.locals.message = err.message;
+	res.locals.error = req.app.get('env') === 'DEV' ? err : {};
+
+	res.status(err.status || 500);
+	res.json({error: {
+		errors: err.errors || [],
+		code: err.status,
+		message: err.message
+	}});
+});
+
+app.listen(app.get('port'), () => 
+	console.log(`Server listening at port: ${app.get('port')}`)
+);
+
+module.exports = app;
