1a81a27bf4e8b0eda125c0753574472acee8f40c
Michael Braga <michael_braga@outlook.com>
2020-02-09 22:16:14 -0500

    fix to make registration links work with Heroku

--- server/utils/express-functions.js
+++ server/utils/express-functions.js
@@ -1,6 +1,7 @@
 const port = process.env.PORT || 3001;
+const isLocal = process.env.LOCAL;
 
-const baseUrl = req => `${req.protocol}://${req.hostname}:${port}}`
+const baseUrl = req => `${req.protocol}://${req.hostname}${isLocal ? `:${port}` : ''}}`
 	.replace('}', '');
 
 module.exports = {baseUrl};
