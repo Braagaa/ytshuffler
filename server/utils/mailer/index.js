6803c206c97501c8319e712655a5bedd46d61ee1
Michael Braga <michael_braga@outlook.com>
2020-09-13 11:07:30 -0400

    Fixed email when registrating. Switch to Amazon SES for emailing.

--- server/utils/mailer/index.js
+++ server/utils/mailer/index.js
@@ -1,7 +1,17 @@
-const nodemailer = require('nodemailer');
+const AWS = require('aws-sdk');
 const path = require('path');
 const {baseUrl} = require('../express-functions');
 
+AWS.config.update({
+	accessKeyId: process.env.AWS_KEY,
+    secretAccessKey: process.env.AWS_SECRET,
+    region: process.env.AWS_REGION
+});
+
+const SES = new AWS.SES();
+
+//add picture with a absolute URL in header tag
+//maybe from S3 eg https://....
 const emailTemplate = url => `
 <!DOCTYPE html>
 <head>
@@ -46,10 +56,9 @@ const emailTemplate = url => `
 	</style>
 </head>
 <body>
+	<header>
+	</header>
 	<div style="background-color: #f06d54; font-family: 'Raleway', sans-serif; max-width: 90%; margin: 3em auto; padding: 2em 0; color: white;">
-		<header>
-			<img style="display: block; margin: 0 auto 2em;" src="cid:titleeltit@ytshuffler.com"/>
-		</header>
 		<div style="font-size: 1.2em; padding: 20px; text-align: center; margin-bottom: 1em;" class="message">Click on the link to complete your registration to YTShuffler. You have 24 hours to complete your registration. If time expires, you may reregister again with the same email.</div>
 		<a href="${url}" style="font-size: 1.5em; text-align: center; margin-bottom: 1em; text-decoration: underline; display: inherit; cursor: pointer;">Click here!</a>
 	</div>
@@ -64,18 +73,26 @@ const config = {
 };
 
 const sendRegistrationEmail = (req, email) => jwt => {
-	const transporter = nodemailer.createTransport(config);
 	const regUrl = (baseUrl(req) + '/auth/register/confirm/' + jwt);
-	return transporter.sendMail({
-		to: email,
-		subject: 'Registration',
-		html: emailTemplate(regUrl),
-		attachments: [{
-			filename: 'title.png',
-			path: path.join(__dirname, 'title.png'),
-			cid: 'titleeltit@ytshuffler.com'
-		}]
-	});
+	return SES.sendEmail({
+		Destination: {
+			ToAddresses: [email]
+		},
+		Message: {
+			Body: {
+				Html: {
+					Charset: 'UTF-8',
+					Data: emailTemplate(regUrl)
+				}
+			},
+			Subject: {
+				Charset: 'UTF-8',
+				Data: 'Registration to YTShuffler'
+			}
+		},
+		Source: process.env.EMAIL
+	})
+		.promise();
 };
 
 module.exports = {sendRegistrationEmail};
